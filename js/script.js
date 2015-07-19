384ffca40bc785497f70fc97b6795b851a7b60b3
bumbu <bmbalex@gmail.com>
2015-07-19 18:03:56 +0300

    Add waves directions

--- js/script.js
+++ js/script.js
@@ -11,9 +11,9 @@ var RenderConfig = {
 , noiseSeed: 100
 , noiseDetalisation: 0.004
 
-, pathInterval: 2.5
+, pathInterval: 5
 , pathLength: 70
-, pathDensity: 3
+, pathDensity: 1
 
 , onChange: function() {
     console.log(arguments)
@@ -58,6 +58,7 @@ var processing = new Processing()
   , pathToFollow = null
   , drawLayer = null
   , isRendering = false
+  , directionLayer = null
 
 paper.setup(canvas);
 
@@ -79,14 +80,48 @@ paper.project.importSVG('images/fish.svg', function() {
   // Create a draw layer
   drawLayer = new paper.Layer()
 
+  initDirectionLayer()
   render()
 })
+
+function initDirectionLayer() {
+  // Create direction layer
+  directionLayer = new paper.Layer()
+  // Init tool
+  new paper.Tool()
+
+  var lastPath = null
+
+  paper.tool.onMouseDown = function(ev) {
+    directionLayer.activate()
+
+    var from = pathToFollow.getNearestPoint(ev.point)
+    lastPath = paper.Path.Line(from, from)
+    lastPath.strokeColor = 'red'
+  }
+
+  paper.tool.onMouseMove = function(ev) {
+    if (lastPath != null) {
+      lastPath.lastSegment.point = ev.point
+    }
+  }
+
+  paper.tool.onMouseUp = function(ev) {
+    if (lastPath != null) {
+      lastPath.lastSegment.point = ev.point
+      lastPath = null
+      render()
+    }
+  }
+
+}
 
 function render() {
   if (drawLayer == null) {return false;}
   if (isRendering) {return false;}
   // Lock
   isRendering = true
+  drawLayer.activate()
 
   // Set processing noise
   processing.noiseSeed(RenderConfig.noiseSeed)
@@ -95,6 +130,9 @@ function render() {
   // Clear draw layer
   drawLayer.removeChildren()
 
+  // Directional Vectors
+  directions = getSortedDirections()
+  maxDirection = getMaxDirection(directions)
 
   var perlinPath, lastPoint, vectorX, vectorY, normalPath
 
@@ -118,16 +156,109 @@ function render() {
     // normalPath = new paper.Path.Line(pathToFollow.getPointAt(offset), pathToFollow.getPointAt(offset).add(pathToFollow.getNormalAt(offset)));
     // normalPath.strokeColor = 'blue';
 
+    var startPoint = pathToFollow.getPointAt(offset)
+      , direction = getDirectionAtOffset(offset, directions)
+      , directionRelative = directionRelativeToMax(direction, maxDirection)
+      // , directionPath = paper.Path.Line(startPoint, startPoint.add(direction))
+    // directionPath.strokeColor = 'green'
+    console.log(directionRelative)
+
+
     for (var step = 0; step < RenderConfig.pathLength; step ++) {
       perlinPath.add(lastPoint)
 
       vectorX = Math.cos(processing.noise(lastPoint.x*RenderConfig.noiseDetalisation,lastPoint.y*RenderConfig.noiseDetalisation) * 2 * Math.PI) * RenderConfig.pathDensity;
       vectorY = -Math.sin(processing.noise(lastPoint.x*RenderConfig.noiseDetalisation,lastPoint.y*RenderConfig.noiseDetalisation) * 2 * Math.PI) * RenderConfig.pathDensity;
-      lastPoint = lastPoint.add([vectorX, vectorY])
+      // lastPoint = lastPoint.add([vectorX, vectorY])
+      lastPoint = lastPoint.add([directionRelative[0] + vectorX * 0.3, directionRelative[1] + vectorY * 0.3])
     }
+
+    // Simplify path
+    perlinPath.simplify()
   }
 
   // Unlock
   isRendering = false
   paper.view.update(true);
+}
+
+function getSortedDirections() {
+  var sortedDirections = directionLayer.children.slice()
+  .map(function(path) {
+    // Cache path offset from path to follow
+    path.offset = pathToFollow.getOffsetOf(path.firstSegment.point)
+    return path
+  })
+  .sort(function(path1, path2){
+    return path1.offset - path2.offset
+  })
+
+  return sortedDirections
+}
+
+function getMaxDirection(directions) {
+  var maxX = 0
+    , maxY = 0
+
+  directions.forEach(function(path) {
+    maxX = Math.max(maxX, Math.abs(path.firstSegment.point.x - path.lastSegment.point.x))
+    maxY = Math.max(maxY, Math.abs(path.firstSegment.point.y - path.lastSegment.point.y))
+  })
+
+  return [maxX, maxY]
+}
+
+paper.Path.prototype.getVector = function() {
+  return [(this.lastSegment.point.x - this.firstSegment.point.x), (this.lastSegment.point.y - this.firstSegment.point.y)]
+}
+
+function getDirectionAtOffset(offset, directions) {
+  var fromDirection = null
+    , toDirection = null
+    , relativePosition = 0
+
+  if (!directions || directions.length == 0) {
+    return [0, 0]
+  } else if (directions.length == 1) {
+    return directions[0].getVector()
+  } else {
+    for (var i = 0; i < directions.length; i++) {
+      if (directions[i].offset > offset) {
+        toDirection = directions[i]
+
+        if (i > 0) {
+          fromDirection = directions[i - 1]
+        } else {
+          fromDirection = directions[directions.length - 1]
+        }
+        break;
+      }
+    }
+
+    if (toDirection == null) {
+      fromDirection = directions[directions.length - 1]
+      toDirection = directions[0]
+    }
+  }
+
+  if (toDirection.offset > fromDirection.offset) {
+    relativePosition = (offset - fromDirection.offset) / (toDirection.offset - fromDirection.offset)
+  } else {
+    var directionsDistance = pathToFollow.length - fromDirection.offset + toDirection.offset
+
+    if (offset >= fromDirection.offset) {
+      relativePosition = (offset - fromDirection.offset) / directionsDistance
+    } else {
+      relativePosition = (offset - fromDirection.offset + pathToFollow.length) / directionsDistance
+    }
+  }
+
+  var directionX = fromDirection.getVector()[0] * (1 - relativePosition) + toDirection.getVector()[0] * relativePosition
+    , directionY = fromDirection.getVector()[1] * (1 - relativePosition) + toDirection.getVector()[1] * relativePosition
+
+  return [directionX, directionY]
+}
+
+function directionRelativeToMax(direction, maxDirection) {
+  return [direction[0] / maxDirection[0] || 0, direction[1] / maxDirection[1] || 0]
 }
